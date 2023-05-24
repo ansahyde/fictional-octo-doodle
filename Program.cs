98e7721b414c6d2ecbd19235e2327a5bd326b40a
Adam Hyde <adam.hyde@ansys.com>
2023-05-24 15:16:54 +0100

    Commit Project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp1
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Prism.Unity;
+    using Prism.Ioc;
+
+    internal interface IExtendedEnumerable<T>
+        : IEnumerable<T>
+    {
+        void DoSomething();
+    }
+
+    internal class ExtendedEnumerableImplementation<T>
+        : IExtendedEnumerable<T>
+    {
+        public void DoSomething()
+        {
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield break;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield break;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var extension = new UnityContainerExtension();
+            _ = extension.Register<IExtendedEnumerable<int>, ExtendedEnumerableImplementation<int>>();
+            _ = extension.Resolve<IExtendedEnumerable<int>>(); // Throws InvalidCastException
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ANSYS")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright © ANSYS 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96dfbf50-c3ba-440f-8059-6cd5c0c2a439")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
